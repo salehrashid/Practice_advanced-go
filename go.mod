ee7009b84be50df3ca82441838517aecc7cedec3
salehrashid <saleh.rashid.b@gmail.com>
2023-09-26 17:19:21 +0700

    add db connector

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-advanced
 
 go 1.21
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ logical/db-builder.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"golang-advanced/constants"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		constants.Host, constants.Port, constants.User, constants.Password, constants.Dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
